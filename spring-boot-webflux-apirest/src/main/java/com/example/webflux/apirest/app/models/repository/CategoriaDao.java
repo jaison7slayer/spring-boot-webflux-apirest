5d6a23a1e902a5623dc0010df2c9178a906a8cd4
jaison7slayer <roejusa7@gmail.com>
2022-04-29 15:48:31 -0500

    borrar
    por error de carga

--- spring-boot-webflux-apirest/src/main/java/com/example/webflux/apirest/app/models/repository/CategoriaDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.webflux.apirest.app.models.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.example.webflux.apirest.app.models.documents.Categoria;
-
-public interface CategoriaDao extends ReactiveMongoRepository<Categoria, String> {
-
-}
--- spring-boot-webflux-apirest/src/main/java/com/example/webflux/apirest/app/models/repository/ProductoDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.webflux.apirest.app.models.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.example.webflux.apirest.app.models.documents.Producto;
-
-public interface ProductoDao extends ReactiveMongoRepository<Producto, String> {
-
-}

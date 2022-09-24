2a2cf4729e2a1065158487db1292627bfffd0e18
jaison7slayer <roejusa7@gmail.com>
2022-09-23 23:03:34 -0500

    Pase de prueba 3

--- src/main/java/com/example/webflux/apirest/app/controllers/ProductoController.java
+++ src/main/java/com/example/webflux/apirest/app/controllers/ProductoController.java
@@ -37,16 +37,12 @@ public class ProductoController {
 		return "Hola, testeando Github Actions con Azure!";
 	}
 	
-	@GetMapping("/test3")
-	public String listarTest2() {
-		return "Hola, testeando para PR prueba nueva!";
-	}
-
 	// ESTA ES LA FORMA MAS SIMPLE Y FACIL
-	/*@GetMapping	
-	 * public Flux<Producto> listar(){ return service.findAll(); }
+	/*	
+	  public Flux<Producto> listar(){ return service.findAll(); }
 	 */
 	// ESTA ES LA FORMA CON MONO Y RESPONSEENTITY
+	@GetMapping
 	public Mono<ResponseEntity<Flux<Producto>>> listar() {
 		return Mono.just(ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON) // APPLICATION_JSON_UTF8
 				.body(service.findAll()));
